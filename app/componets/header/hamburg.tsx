6b7160cca3d022cea783842b22ce3da7e503d2ba
james-script <jamessonlucas01@gmail.com>
2026-03-13 17:38:19 -0300

    feature: sidebar

---
+++ app/componets/header/hamburg.tsx
@@ -0,0 +1,86 @@
+"use client";
+
+import { useState, useEffect } from "react";
+import { Menu, X } from "lucide-react";
+import Link from "next/link";
+
+export default function Hamburg() {
+  const [open, setOpen] = useState(false);
+
+  useEffect(() => {
+    if (open) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "auto";
+    }
+
+    return () => {
+      document.body.style.overflow = "auto";
+    };
+  }, [open]);
+
+  return (
+    <>
+      {/* botão */}
+      <div className="flex md:hidden justify-end w-full relative z-[70]">
+        <button onClick={() => setOpen(!open)} className="cursor-pointer mr-4">
+          {open ? <X className="text-black" size={30} /> : <Menu size={30} />}
+        </button>
+      </div>
+
+      {/* overlay */}
+      <div
+        onClick={() => setOpen(false)}
+        className={`fixed inset-0 bg-black/40 z-[40] transition-opacity duration-300 md:hidden ${
+          open ? "opacity-100 pointer-events-auto" : "opacity-0 pointer-events-none"
+        }`}
+      />
+
+      {/* sidebar */}
+      <div
+        className={`fixed top-0 right-0 h-screen w-50 bg-[#f06d05] z-[60] md:hidden
+        transition-transform duration-300 ease-in-out
+        ${open ? "translate-x-0" : "translate-x-full"}`}
+      >
+        <div className="flex justify-end p-4">
+          <button onClick={() => setOpen(false)} className="cursor-pointer">
+        
+          </button>
+        </div>
+
+        <nav className="flex flex-col items-center gap-6 ml-[-16] mt-12">
+          <Link
+            href="#"
+            className="w-[85%] rounded-3xl rounded-s-none bg-[#fff8cd] px-4 py-2 text-black shadow-xl transition-all duration-300 hover:bg-[#fbe97d] hover:shadow-2xl"
+            onClick={() => setOpen(false)}
+          >
+            Início
+          </Link>
+
+          <Link
+            href="#"
+            className="w-[85%] rounded-3xl rounded-s-none bg-[#fff8cd] px-4 py-2 text-black shadow-xl transition-all duration-300 hover:bg-[#fbe97d] hover:shadow-2xl"
+            onClick={() => setOpen(false)}
+          >
+            Sobre
+          </Link>
+
+          <Link
+            href="#"
+            className="w-[85%] rounded-3xl rounded-s-none bg-[#fff8cd] px-4 py-2 text-black shadow-xl transition-all duration-300 hover:bg-[#fbe97d] hover:shadow-2xl"
+            onClick={() => setOpen(false)}
+          >
+            Projetos
+          </Link>
+
+          <button
+            className="mt-8 w-[85%] rounded-3xl bg-[#fff8cd] ml-[16] px-4 py-2 text-black shadow-xl transition-all duration-300 hover:bg-[#fbe97d] hover:shadow-2xl"
+            onClick={() => setOpen(false)}
+          >
+            Contato
+          </button>
+        </nav>
+      </div>
+    </>
+  );
+}
